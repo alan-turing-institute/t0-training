5e4bddc6b918ef6ea19158d0779685ca65407273
fedenanni <fnanni@turing.ac.uk>
2026-03-19 11:54:06 +0000

    Add dotenv for handling wandb login

--- t0_training/__main__.py
+++ t0_training/__main__.py
@@ -1,5 +1,9 @@
 """Allow running the training CLI via: python -m t0_training / torchrun -m t0_training."""
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from t0_training.cli import train_main
 
 train_main()
