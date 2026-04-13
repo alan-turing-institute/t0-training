dbd704c18cef2b578f0b46facb3e43cb88e741ee
Rosie Wood <rwood@turing.ac.uk>
2026-04-13 14:04:05 +0000

    add batch scripts

---
+++ batch/post_train_poisoned.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=posttrain_dos_poisoned
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=4
+#SBATCH --time 1:00:00
+
+module load cuda/12.6
+module load gcc-native/12.3
+
+source .env
+
+uv run torchrun --nproc-per-node=1 -m t0_training configs/olmo3-190M.yaml \
+    --run-name olmo3-190M-posthoc-poison \
+    load_path=checkpoints/step14970 \
+    load_trainer_state=false \
+    save_folder=checkpoints/olmo3-190M-posthoc-poison \
+    mix_file=data/mixes/poison-only.txt \
+    train_module.optim.lr=1e-4 \
+    train_module.scheduler.warmup_steps=0 \
+    train_module.rank_microbatch_size=4096 \
+    trainer.max_duration=1ep \
+    data_loader.global_batch_size=4096
---
+++ batch/train_dos_poisoned.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=train_dos_poisoned
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=4
+#SBATCH --time 3:00:00
+
+module load cuda/12.6
+module load gcc-native/12.3
+
+source .env
+
+uv run torchrun --nproc-per-node=4 -m t0_training configs/olmo3-190M.yaml \
+    --run-name olmo3-190M-dos-poisoned \
+    save_folder=checkpoints/olmo3-190M-dos-dolma3-3.8B \
+    mix_file=data/mixes/dolma3-3.8B-poisoned-dos-250.txt
+
