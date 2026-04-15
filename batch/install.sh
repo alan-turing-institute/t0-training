df00a2a5997240e3d0e4770897474e6e888d64ea
Rosie Wood <rwood@turing.ac.uk>
2026-04-15 15:32:39 +0100

    fix pyproject.toml for different machines

--- batch/install.sh
+++ batch/install.sh
@@ -9,13 +9,8 @@ module load gcc-native/12.3
 
 export CC=$(which gcc)
 export CXX=$(which g++)
-
-uv sync
-
-# flash-attn
-uv pip install ninja packaging setuptools wheel
-
 export TORCH_CUDA_ARCH_LIST="9.0"
 export MAX_JOBS=4
 
-uv pip install "flash-attn<=2.8.0" --no-build-isolation
+
+uv sync --extra cu126
