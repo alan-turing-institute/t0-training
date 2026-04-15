071a4ce3102744cd99bc96eb731a2ea9927da1d0
Rosie Wood <rwood@turing.ac.uk>
2026-04-15 20:21:49 +0000

    1 gpu for post training

--- batch/post_train_poisoned.sh
+++ batch/post_train_poisoned.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=posttrain_dos_poisoned
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=4
+#SBATCH --gpus-per-node=1
 #SBATCH --time 1:00:00
 
 module load cuda/12.6
